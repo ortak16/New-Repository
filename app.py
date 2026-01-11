d4b08f8c095909fa2dc5b99f9e3d3169a9ed0e99
ortak16 <odbbtu@gmail.com>
2026-01-11 03:34:57 +0300

    app.py

--- app.py
+++ app.py
@@ -93,7 +93,7 @@ else:
 @st.cache_resource
 def get_model():
     return genai.GenerativeModel(
-        model_name="gemini-flash-latest",
+        model_name="gemini-2.5-flash-native-audio-dialog",
         system_instruction=final_instruction
     )
 
@@ -168,3 +168,4 @@ if len(st.session_state.messages) == 0:
         if st.button("Eleştirel Düşünme Yöntemleri/Yapay Zeka Dersleri"):
             st.session_state.pending_prompt = "Eleştirel Düşünme Yöntemleri/Yapay Zeka Derslerini sisteminizde göremiyorum?"
             st.rerun()
+
